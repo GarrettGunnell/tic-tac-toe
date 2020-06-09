063dc881534ce305696b15dc2018ce39ba3b0a1f
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-06-09 09:06:40 -0700

    A game is not initially over

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,15 @@ require_relative 'board'
 class Game
 
   def initialize(board = nil)
+    @over = false
     @board = board;
   end
 
   def board
     @board
+  end
+
+  def over?
+    @over
   end
 end
